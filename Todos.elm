934e80415387536a23f81d905f83a79d9e04389e
Grégory Fouquet <gfouquet@armida.fr>
2016-09-25 00:03:18 +0200

    Todos are togglable

--- Todos.elm
+++ Todos.elm
@@ -1,28 +1,39 @@
 module Todos exposing (..)
 
-import Html exposing(Html, button, div, text, a, ul, li, input)
+import Html exposing (Html, button, div, text, a, ul, li, input)
 import Html.App as App
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
+import Debug exposing (log)
+
 
 -- MAIN
-main : Program Never
 main = App.beginnerProgram { model = model, view = view, update = update }
 
 -- MODEL
+type alias Model = List Todo
+model = [
+    { label = "Learn Elm", done = False}
+    , { label = "Gouge away", done = True}]
 
-type alias Model = String
-model = "Learn Elm"
+type alias Todo = { label: String, done: Bool }
 
 -- UPDATE
-
-type Msg = NoOp
+type Msg = NoOp | ToggleDone Int
 
 update : Msg -> Model -> Model
 update msg model =
-    case msg of
+    case log "msg" msg of
         NoOp -> model
+        ToggleDone dx -> toggleDone dx model
+
+toggleDone dx model =
+    List.indexedMap
+        ( \n todo ->
+            if n == dx then { todo | done = not todo.done }
+            else todo )
+        model
 
 -- VIEW
 
@@ -34,12 +45,10 @@ view model =
             input [ placeholder "Todo" ] []
             , button [] [text "Add"]
         ]
-        , ul [] [
-            li [] [text model]
-        ]
+        , ul [] (List.indexedMap todoToLiMapper model)
         , div []
         [
-            a[] [text "All"]
+            a[style [("font-weight", "bold")]] [text "All"]
             , text " "
             , a[] [text "Pending"]
             , text " "
@@ -47,3 +56,13 @@ view model =
 
         ]
     ]
+
+todoToLiMapper dx todo =
+    li [style [( "text-decoration", liDecoration todo )]
+        , onClick (ToggleDone dx) ]
+        [text todo.label]
+
+liDecoration todo =
+    case todo.done of
+        True -> "line-through"
+        False -> "none"
