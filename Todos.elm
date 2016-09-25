6f383d70987272ecb03dc0e75549e3a4b1d2917e
Grégory Fouquet <gfouquet@armida.fr>
2016-09-25 21:57:31 +0200

    Add button adds content of input

--- Todos.elm
+++ Todos.elm
@@ -3,65 +3,87 @@ module Todos exposing (..)
 import Html exposing (Html, button, div, text, a, ul, li, input)
 import Html.App as App
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (onClick, onInput)
 
 import Debug exposing (log)
 
+import String exposing (length)
 
 -- MAIN
 main = App.beginnerProgram { model = model, view = view, update = update }
 
 -- MODEL
-type alias Model = List Todo
-model = [
-    Todo "Learn Elm" False
-    , Todo "Gouge away" True]
+type alias Model = { todos : List Todo, input : String, filter : Filter }
+model =
+    { todos =
+        [ Todo "Learn Elm" False
+        , Todo "Gouge away" True
+        ]
+    , input = ""
+    , filter = FilterAll
+    }
 
 type alias Todo = { label: String, done: Bool }
 
+type Filter = FilterAll | FilterPending | FilterDone
+
 -- UPDATE
-type Msg = NoOp | ToggleDone Int | Add String
+type Msg = NoOp | ToggleDone Int | AddTodo String | InputTodo String
 
 update : Msg -> Model -> Model
 update msg model =
     case log "msg" msg of
         NoOp -> model
-        ToggleDone dx -> toggleDone dx model
-        Add label -> todo label :: List.reverse model |> List.reverse
+        ToggleDone dx -> { model | todos = toggleDone dx model.todos }
+        AddTodo label ->
+            { model
+            | todos = todo label :: List.reverse model.todos |> List.reverse
+            , input = ""
+            }
+        InputTodo label -> { model | input = label }
 
-toggleDone dx model =
+toggleDone dx todos =
     List.indexedMap
         ( \n todo ->
             if n == dx then { todo | done = not todo.done }
-            else todo )
-        model
+            else todo
+        )
+        todos
 
 todo label =
-    { label = label, done = False }
+  { label = label, done = False }
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-    div [] [
-        div []
-        [
-            input [ placeholder "Todo" ] []
-            , button [onClick (Add "Velouria")] [text "Add"]
-        ]
-        , ul [] (List.indexedMap todoToLiMapper model)
+    div []
+        [ div []
+            [ input
+                [ placeholder "Todo"
+                , onInput InputTodo
+                , value model.input
+                ] []
+            , button
+                [ onClick (AddTodo model.input)
+                , disabled (log "input length" String.length model.input == 0)
+                ]
+                [text "Add"]
+            ]
+        , ul [] (List.indexedMap todoToLiMapper model.todos)
         , div []
-        [
-            a[style [("font-weight", "bold")]] [text "All"]
+            [ a [style [("font-weight", "bold")]] [text "All"]
             , text " "
             , a[] [text "Pending"]
             , text " "
             , a[] [text "Done"]
+            ]
         ]
-    ]
 
 todoToLiMapper dx todo =
-    li [style [( "text-decoration", liDecoration todo )]
-        , onClick (ToggleDone dx) ]
+    li
+        [style [( "text-decoration", liDecoration todo )]
+        , onClick (ToggleDone dx)
+        ]
         [text todo.label]
 
 liDecoration todo =
